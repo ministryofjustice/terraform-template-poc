643092afa9f7425b840bdfecb87c83dbde4100f9
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2023-03-31 10:17:19 +0100

    ✨ Add terraform plan step to PRs (#5)

---
+++ backend.hcl
@@ -0,0 +1,3 @@
+encrypt="true"
+bucket="cloud-platform-7a481a8e1fa4ad1cca341e9247fcfdb6"
+dynamodb_table="cp-0f78cd437cb76b69"
---
+++ terraform/github/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {}
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = " ministryofjustice-test"
+}
+
+resource "github_membership" "membership_for_nick" {
+  username = "NickWalt01"
+  role     = "member"
+}
